87ee195b365a4aaa43717b886c0a9e77947c02b2
Emma Lennon <e.lennon6@universityofgalway.ie>
2023-12-21 15:48:12 +0000

    Messed around with the UI to get a rough idea of the page.

--- app.R
+++ app.R
@@ -2,39 +2,73 @@ library(shiny)
 library(shinydashboard)
 library(tidyverse)
 dig.df = read.csv("DIG.csv")
+# To do: tidy the data for a more readable format - later
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
-    )
-  ),
-  ## Body content
-  dashboardBody(
-    tabItems(
-      # First tab content
-      tabItem(tabName = "dashboard",
-              fluidRow(
-                box(plotOutput("plot1", height = 250)),
-                
-                box(
-                  title = "Controls",
-                  sliderInput("slider", "Number of observations:", 1, 100, 50)
-                )
+
+## UI
+# Header
+header <- dashboardHeader(title = "DIG Data")
+
+# Sidebar
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+  )
+)
+
+# Body
+body <- dashboardBody(
+  fluidRow(
+    # A static valueBox
+    valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
+    # Dynamic valueBoxes
+    valueBoxOutput("progressBox"),
+    valueBoxOutput("approvalBox")),
+  
+  tabItems(
+    # First tab content
+    tabItem(tabName = "dashboard",
+            fluidRow(
+              box(title = "Histogram", background = "aqua", solidHeader = TRUE,
+                  collapsible = TRUE, plotOutput("plot1", height = 600)),
+              
+              box(title = "Inputs", background = "aqua", solidHeader = TRUE, collapsible = TRUE,
+                  "Box content here", br(), "More box content",
+                  sliderInput("slider", "Slider input:", 1, 100, 50),
+                  textInput("text", "Text input:")
               )
-      ),
-      
-      # Second tab content
-      tabItem(tabName = "widgets",
-              h2("Widgets tab content")
-      )
+            )
+    ),
+    
+    # Second tab content
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
     )
   )
 )
 
+
+ui <- dashboardPage(skin = "red", header, sidebar, body)
+
+
+## Server
+
 server <- function(input, output) {
+  output$progressBox <- renderValueBox({
+    valueBox(
+      paste0(25 + 50, "%"), "Progress", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$approvalBox <- renderValueBox({
+    valueBox(
+      "80%", "Approval", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
   set.seed(122)
   histdata <- rnorm(500)
   
@@ -44,4 +78,6 @@ server <- function(input, output) {
   })
 }
 
+
+## Run
 shinyApp(ui, server)

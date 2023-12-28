44d308ba43bb1499d65cc940573359ceeec495f4
Keelan Eyers <eyersk@gmail.com>
2023-12-28 22:16:59 +0000

    Updated barcharts

--- app.R
+++ app.R
@@ -47,6 +47,8 @@ body <- dashboardBody(
   tabItems(
     # First tab content
     tabItem(tabName = "Boxplots",
+            h2("DIG Data Overview"),
+            
             # value boxes for summary: Treatment group
             fluidRow(valueBoxOutput("treatmentNo1"), valueBoxOutput("maleProp1"), valueBoxOutput("meanAge1"),
                      valueBoxOutput("meanBMI1"), valueBoxOutput("meanDBP1"), valueBoxOutput("meanSBP1")),
@@ -58,20 +60,35 @@ body <- dashboardBody(
             fluidRow(
               
               box(title = "Inputs", background = "purple", solidHeader = T, collapsible = T,
-                  selectInput("box_var", "Select Column for Boxplot:", choices = names(dig.df)[c(3, 6, 7, 8, 9)]),
-                  selectInput("binary_var", "Select Binary Column for Grouping:", choices = names(dig.df)[c(2, 4, 5)])),
-              
+                  selectInput("box_var", "Select continuous variable:", choices = names(dig.df)[c(3, 6, 7, 8, 9)]),
+                  selectInput("binary_var", "Group by:", choices = names(dig.df)[c(2, 4, 5, 10:20)]))),
+            
+            fluidRow(
               box(title = "Boxplot", background = "purple", solidHeader = TRUE,
-                  collapsible = TRUE, plotOutput("plot1", height = 600))
+                  collapsible = TRUE, plotOutput("plot1", height = 600)),
+              
+              box(title = "Density Plot", background = "purple", solidHeader = TRUE,
+                  collapsible = TRUE, plotOutput("plot3", height = 600))
             )
     ),
     
     # Second tab content
     tabItem(tabName = "Barcharts",
-            h2("Widgets tab content")
+            h2("Barcharts for DIG Data"),
+            
+            # plots
+            fluidRow(
+              
+              box(title = "Inputs", background = "purple", solidHeader = T, collapsible = T,
+                  selectInput("outcome_var", "Select variable:", choices = names(dig.df)[c(2, 4, 5, 10:20)]),
+                  selectInput("group_var", "Group by:", choices = names(dig.df)[c(2, 4, 5, 10:20)])),
+              
+              box(title = "Barchart", background = "purple", solidHeader = TRUE,
+                  collapsible = TRUE, plotOutput("plot2", height = 600))
+            )
     )
-  )
-)
+    
+  ))
 
 ui <- dashboardPage(skin = "red", header, sidebar, body)
 
@@ -81,98 +98,117 @@ ui <- dashboardPage(skin = "red", header, sidebar, body)
 server <- function(input, output) {
   output$treatmentNo1 <- renderValueBox({
     valueBox(
-      paste0(length(dig.df$TRTMT[dig.df$TRTMT == 1])), "In Treatment Group", icon = icon("list"),
+      paste0(length(dig.df$Treatment[dig.df$Treatment == "Treatment"])), "In Treatment Group", icon = icon("list"),
       color = "purple"
     )
   })
   
   output$treatmentNo0 <- renderValueBox({
     valueBox(
-      paste0(length(dig.df$TRTMT[dig.df$TRTMT == 0])), "In Placebo Group", icon = icon("list"),
+      paste0(length(dig.df$Treatment[dig.df$Treatment == "Placebo"])), "In Placebo Group", icon = icon("list"),
       color = "yellow"
     )
   })
   
   output$maleProp1 <- renderValueBox({
     valueBox(
-      paste0(dig.df %>% filter(TRTMT == 1 & SEX == 1) %>% count()), "Males in Treatment", icon = icon("list"),
+      paste0(dig.df %>% filter(Treatment == "Treatment" & Sex == "Male") %>% count()), "Males in Treatment", icon = icon("list"),
       color = "purple"
     )
   })
-    
-    output$maleProp0 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% filter(TRTMT == 0 & SEX == 1) %>% count()), "Males in Placebo", icon = icon("list"),
-               color = "yellow"
-        )
-    })
-    
-    output$meanAge1 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, AGE) %>% filter(TRTMT == 1) %>% summarise(mean = round(mean(AGE), 1))), "Mean Age of Treatment", icon = icon("list"),
-               color = "purple"
-        )
-    })
-    
-    output$meanAge0 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, AGE) %>% filter(TRTMT == 0) %>% summarise(mean = round(mean(AGE), 1))), "Mean Age of Placebo", icon = icon("list"),
-        color = "yellow"
-      )
-    })
-    
-    output$meanBMI1 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, BMI) %>% filter(TRTMT == 1) %>% summarise(mean = round(mean(BMI), 1))), "Mean BMI of Treatment", icon = icon("list"),
-        color = "purple"
-      )
-    })
-    
-    output$meanBMI0 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, BMI) %>% filter(TRTMT == 0) %>% summarise(mean = round(mean(BMI, na.rm = T), 1))), "Mean BMI of Placebo", icon = icon("list"),
-        color = "yellow"
-      )
-    })
-    
-    output$meanDBP1 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, DIABP) %>% filter(TRTMT == 1) %>% summarise(mean = round(mean(DIABP, na.rm = T), 1))), "Mean Diastolic BP of Treatment", icon = icon("list"),
-        color = "purple"
-      )
-    })
-    
-    output$meanDBP0 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, DIABP) %>% filter(TRTMT == 0) %>% summarise(mean = round(mean(DIABP, na.rm = T), 1))), "Mean Diastolic BP of Placebo", icon = icon("list"),
-        color = "yellow"
-      )
-    })
-    
-    output$meanSBP1 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, SYSBP) %>% filter(TRTMT == 1) %>% summarise(mean = round(mean(SYSBP, na.rm = T), 1))), "Mean Systolic BP of Treatment", icon = icon("list"),
-        color = "purple"
-      )
-    })
-    
-    output$meanSBP0 <- renderValueBox({
-      valueBox(
-        paste0(dig.df %>% select(TRTMT, SYSBP) %>% filter(TRTMT == 0) %>% summarise(mean = round(mean(SYSBP, na.rm = T), 1))), "Mean Systolic BP of Placebo", icon = icon("list"),
-        color = "yellow"
-      )
-    })
+  
+  output$maleProp0 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% filter(Treatment == "Placebo" & Sex == "Male") %>% count()), "Males in Placebo", icon = icon("list"),
+      color = "yellow"
+    )
+  })
+  
+  output$meanAge1 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Age) %>% filter(Treatment == "Treatment") %>% summarise(mean = round(mean(Age), 1))), "Mean Age of Treatment", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$meanAge0 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Age) %>% filter(Treatment == "Placebo") %>% summarise(mean = round(mean(Age), 1))), "Mean Age of Placebo", icon = icon("list"),
+      color = "yellow"
+    )
+  })
+  
+  output$meanBMI1 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, BMI) %>% filter(Treatment == "Treatment") %>% summarise(mean = round(mean(BMI), 1))), "Mean BMI of Treatment", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$meanBMI0 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, BMI) %>% filter(Treatment == "Placebo") %>% summarise(mean = round(mean(BMI, na.rm = T), 1))), "Mean BMI of Placebo", icon = icon("list"),
+      color = "yellow"
+    )
+  })
+  
+  output$meanDBP1 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Diastolic_BP) %>% filter(Treatment == "Treatment") %>% summarise(mean = round(mean(Diastolic_BP, na.rm = T), 1))), "Mean Diastolic BP of Treatment", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$meanDBP0 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Diastolic_BP) %>% filter(Treatment == "Placebo") %>% summarise(mean = round(mean(Diastolic_BP, na.rm = T), 1))), "Mean Diastolic BP of Placebo", icon = icon("list"),
+      color = "yellow"
+    )
+  })
+  
+  output$meanSBP1 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Systolic_BP) %>% filter(Treatment == "Treatment") %>% summarise(mean = round(mean(Systolic_BP, na.rm = T), 1))), "Mean Systolic BP of Treatment", icon = icon("list"),
+      color = "purple"
+    )
+  })
+  
+  output$meanSBP0 <- renderValueBox({
+    valueBox(
+      paste0(dig.df %>% select(Treatment, Systolic_BP) %>% filter(Treatment == "Placebo") %>% summarise(mean = round(mean(Systolic_BP, na.rm = T), 1))), "Mean Systolic BP of Placebo", icon = icon("list"),
+      color = "yellow"
+    )
+  })
   
   output$plot1 <- renderPlot({
     ggplot(dig.df, aes_string(x = input$binary_var, y = input$box_var, fill = input$binary_var)) +
-      geom_boxplot() +
+      geom_boxplot(alpha = 0.9) +
       labs(title = paste("Boxplot of", input$box_var, "grouped by", input$binary_var),
            x = input$binary_var, y = input$box_var) +
       theme_minimal() +
       scale_fill_manual(values = c("orange", "cornflowerblue"))
   })
+  
+  output$plot2 <- renderPlot({
+    ggplot(dig.df, aes_string(x = input$group_var, fill = input$outcome_var)) +
+      geom_bar(position = "fill") +
+      labs(title = paste("Barchart of", input$outcome_var, "by", input$group_var),
+           x = input$group_var, y = "Proportion") +
+      theme_minimal() +
+      scale_fill_manual(values = c("lightgreen", "coral3"))
+  })
+  
+  output$plot3 <- renderPlot({
+    ggplot(dig.df, aes_string(x = input$box_var, fill = input$binary_var)) +
+      geom_density(alpha = 0.9) +
+      facet_wrap(~ get(input$binary_var)) +
+      labs(title = paste("Density Plot of", input$box_var, "grouped by", input$binary_var),
+           x = input$box_var, y = "Density") +
+      theme_minimal() +
+      scale_fill_manual(values = c("orange", "cornflowerblue"))
+  })
 }
-#Testing commit
+
 
 ## Run
 shinyApp(ui, server)
